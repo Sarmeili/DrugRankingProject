3fe41973878ce93af5896568bcab502950850ce1
Faraz Sarmeili <sarmeili.f@gmail.com>
2023-10-27 07:34:43 +0330

    Fixing memory usage problem

--- datahandler/ctrp_drugranker.py
+++ datahandler/ctrp_drugranker.py
@@ -19,7 +19,7 @@ class CTRPHandler:
         if self.source == 'DrugRanker':
             self.cmpd_df = pd.read_csv('data/ctrp_drugranker/cmpd_id_name_group_smiles.txt', sep='\t')
             self.reponse_df = pd.read_csv('data/ctrp_drugranker/final_list_auc.txt')
-            self.reponse_df = self.reponse_df[:int(len(self.reponse_df) * self.data_volume)]
+            self.reponse_df = self.reponse_df[int(len(self.reponse_df) * self.data_volume[0]):int(len(self.reponse_df) * self.data_volume[1])]
             self.exp_ccl_df = pd.read_csv('data/CCLE_expression.csv', index_col=0)
 
     def create_tensor_feat_cll(self, is_pca=True):
--- train.py
+++ train.py
@@ -5,11 +5,12 @@ from modelutils.training import TrainModel
 from modelutils.evaluation import EvaluateModel
 from torch.utils.data import DataLoader
 import torch
+import numpy as np
 import warnings
 
 warnings.filterwarnings('ignore')
 
-data = CTRPHandler('DrugRanker', 0.3)
+'''data = CTRPHandler('DrugRanker', [0, 0.01])
 train_dataset = CTRPDatasetTorch(data, True)
 train_dataloader = DataLoader(train_dataset, batch_size=64)
 test_dataset = CTRPDatasetTorch(data, False)
@@ -32,4 +33,40 @@ for test_X, test_label in test_dataloader:
     print('y_label', test_label)
     evaluator = EvaluateModel(trained_model, test_cll, test_drug, test_label, loss_fn)
     final_loss = evaluator.evaluate()
-    print('Final Loss : ' + str(final_loss))
+    print('Final Loss : ' + str(final_loss))'''
+
+data = CTRPHandler('DrugRanker', [0, 0.01])
+train_dataset = CTRPDatasetTorch(data, True)
+model = Model1(train_dataset[0][0][0].shape[0], train_dataset[0][0][1].shape[0])
+model = model.to('cuda')
+loss_fn = torch.nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+num_epochs = 100
+del data
+del train_dataset
+for i in np.arange(0, 1, 0.01):
+
+    print(i)
+    data = CTRPHandler('DrugRanker', [i, i + 0.01])
+    train_dataset = CTRPDatasetTorch(data, True)
+    train_dataloader = DataLoader(train_dataset, batch_size=64)
+    test_dataset = CTRPDatasetTorch(data, False)
+    test_dataloader = DataLoader(test_dataset, batch_size=len(test_dataset))
+    trainer = TrainModel(model, train_dataloader, loss_fn, optimizer, num_epochs)
+    model = trainer.train_model()
+    for test_X, test_label in test_dataloader:
+        test_cll, test_drug = test_X
+        y_pred = model(test_cll, test_drug)
+        model.eval()
+        print('y_pred', y_pred)
+        print('y_label', test_label)
+        evaluator = EvaluateModel(model, test_cll, test_drug, test_label, loss_fn)
+        final_loss = evaluator.evaluate()
+        print('Final Loss : ' + str(final_loss))
+    del data
+    del train_dataset
+    del train_dataloader
+    del test_dataset
+    del test_dataloader
+
+torch.save(model, 'models/simple_mpl.pt')
